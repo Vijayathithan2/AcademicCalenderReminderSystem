aa9008d7298ba97e5e5c2ab9caffcf15ac52a540
Vijayathithan2 <adhi822007@gmail.com>
2025-11-13 16:40:55 +0530

    Add files via upload

---
+++ CalendarAlertApplication.java
@@ -0,0 +1,11 @@
+package com.academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAlertApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarAlertApplication.class, args);
+    }
+}
